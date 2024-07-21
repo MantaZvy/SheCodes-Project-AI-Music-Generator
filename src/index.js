f88f6f9693d7c29cbd874368101466f9f3ed21bd
MantaZvy <mantaszvy@gmail.com>
2024-07-21 19:54:01 +0100

    Added AI API Integration,

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generateMusicSuggestions(event) {
-  event.preventDefault();
-
+function displayMusicSuggestions(response) {
   new Typewriter("#music", {
-    strings: "Hello there, this is a music suggestion",
+    strings: response.data.answer,
     autoStart: true,
     delay: 2,
     cursor: "",
   });
+}
+
+function generateMusicSuggestions(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "fcd20cbab033a83d4912e96fcoedf8t4";
+  let prompt = `User instructions: ${instructionsInput.value}`;
+  let context =
+    "You are a music expert who knows all songs from every genre and love to suggest great songs. Your mission is to generate any 5 song suggestions based on user instructions. Make sure to follow user instructions and also give new line for each song in html <br> format, thank you!.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("generating poem");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiUrl).then(displayMusicSuggestions);
 }
 
 let musicButton = document.querySelector("#music-generator-form");
